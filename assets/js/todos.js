81829bf9bb8865d9702bbd6269598d98b571f290
schhizy <iit2019141@iiita.ac.in>
2021-01-06 23:14:10 +0530

    adding files

---
+++ assets/js/todos.js
@@ -0,0 +1,26 @@
+
+$("ul").on("click", "li", function(){
+    $(this).toggleClass("completed");
+});
+
+//Click on X to delete Todo
+ $("ul").on("click", "span", function(event){
+    $(this).parent().fadeOut(500,function(){
+        $(this).remove();
+    });
+    event.stopPropagation();
+ });
+
+ $("input[type='text']").keypress(function(event){
+    if(event.which === 13){
+        //grabbing new todo
+        var todoText = $(this).val();
+        $(this).val("");
+        //create new todo
+        $("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>")
+    }
+ });
+
+ $(".fa-plus").click(function(){
+     $("input[type='text']").fadeToggle();
+ });
